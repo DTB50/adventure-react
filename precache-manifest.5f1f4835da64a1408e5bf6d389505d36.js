62e779d6a61cfafc8b7b0eb69a8c2988a24bb66c
DavidB <david.burns.dtb@googlemail.com>
2019-04-25 15:02:35 +0100

    Updates

---
+++ precache-manifest.5f1f4835da64a1408e5bf6d389505d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36739c9e7841d3ce554",
+    "url": "/adventure-react/static/js/runtime~main.859fc518.js"
+  },
+  {
+    "revision": "5a592da044d7d97c7f94",
+    "url": "/adventure-react/static/js/main.3768c959.chunk.js"
+  },
+  {
+    "revision": "4bd1d4eea210ddd5bfad",
+    "url": "/adventure-react/static/js/2.ea93b2ed.chunk.js"
+  },
+  {
+    "revision": "5a592da044d7d97c7f94",
+    "url": "/adventure-react/static/css/main.2171fc03.chunk.css"
+  },
+  {
+    "revision": "3be6d0276441309ae2bd06d86630334e",
+    "url": "/adventure-react/index.html"
+  }
+];
