9b5f52de68db53c03f01014af4a2b52cfb225958
DavidB <david.burns.dtb@googlemail.com>
2019-07-15 15:29:05 +0100

    Updates

---
+++ precache-manifest.e05bf7fa3eb9515bbb29c9245eecd870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc8a0f1e5c05176f3d4f16722ae7a37",
+    "url": "/adventure-react/index.html"
+  },
+  {
+    "revision": "33e156ab9aa1dd129c09",
+    "url": "/adventure-react/static/css/main.4abfd2c7.chunk.css"
+  },
+  {
+    "revision": "4bd1d4eea210ddd5bfad",
+    "url": "/adventure-react/static/js/2.ea93b2ed.chunk.js"
+  },
+  {
+    "revision": "33e156ab9aa1dd129c09",
+    "url": "/adventure-react/static/js/main.2924c187.chunk.js"
+  },
+  {
+    "revision": "e36739c9e7841d3ce554",
+    "url": "/adventure-react/static/js/runtime~main.859fc518.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adventure-react/precache-manifest.7fd8cb8a556d7dc53444d0d602bcbe2c.js"
+  "/adventure-react/precache-manifest.e05bf7fa3eb9515bbb29c9245eecd870.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/adventure-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adventure-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
